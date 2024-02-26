ac57d2921b43b2f871d2f400fb96f8d5f7e937b9
shashank-pd <shashankpd2606@gmail.com>
2024-02-26 16:30:14 +0530

    deleted unnecessary files

--- script/time.js
+++
@@ -1,6 +0,0 @@
-
-
-      a = new Date();
-      let date = a.toLocaleDateString();
-      let time = a.toLocaleTimeString();
-      document.getElementById("time").innerHTML = time + " On " + date;
